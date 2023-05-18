a5ed26ace93138f8f48cea9c110be275cd300b5d
chiclaim <chiclaim@gmail.com>
2023-05-18 19:15:31 +0800

    column widget demo1

--- lib/multi-child/column_demo_page.dart
+++ lib/multi-child/column_demo_page.dart
@@ -9,8 +9,14 @@ class ColumnDemoPage extends BaseStatelessPage {
   Widget buildBody() {
     return Container(
         color: Colors.blue,
+        // width: double.infinity, // 宽度 match-parent
+        // Layout a list of child widgets in the vertical direction.
+        // mainAxis = vertical direction
         child: Column(
+          // 侧轴居中
           crossAxisAlignment: CrossAxisAlignment.center,
+          // 默认 mainAxisSize=MainAxisSize.max：沿着主轴mainAxis方向，占用最大的可用空间
+          mainAxisSize: MainAxisSize.min,
           children: const <Widget>[
             Text("hi"),
             Text("world"),
